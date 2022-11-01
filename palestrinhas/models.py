609a233e54730c7dc1868f888ff491d402c002b5
Carine Neris <carine_neris@hotmail.com>
2022-11-01 16:13:53 -0300

    criado model de cadastro de palestra

--- palestrinhas/models.py
+++ palestrinhas/models.py
@@ -1,3 +1,21 @@
+from email.policy import default
+from pyexpat import model
+from secrets import choice
+from statistics import mode
 from django.db import models
 
-# Create your models here.
+
+STATUS_CHOICES = [
+    ('JV', 'Já Vi'),
+    ('QV', 'Quero Ver')
+]
+
+
+class Palestras(models.Model):
+    nome = models.CharField(max_length=150)
+    link = models.URLField()
+    descricao = models.CharField(max_length=400)
+    data_criacao = models.DateField()
+    status = models.CharField(max_length = 2,
+                                choice=STATUS_CHOICES,
+                                default='Já Vi')

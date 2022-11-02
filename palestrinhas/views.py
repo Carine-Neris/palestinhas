4c97988e8522d9d0f7ccc0cfaa1c5022e19aef36
Carine Neris <carine_neris@hotmail.com>
2022-11-02 12:04:47 -0300

    criado view para listar as palestras cadastradas

--- palestrinhas/views.py
+++ palestrinhas/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from .models import Palestras
 
+def palestras_list(request):
+    palestras = Palestras.objects.all()
+    context = {'context':palestras}
 
-def index(request):
-    return render(request, 'palestras/index.html')
+    return render(request,'palestras/index.html',context)
+  

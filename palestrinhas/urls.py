7710ff04de37463d69d7ad1789039582daa0b030
Carine Neris <carine_neris@hotmail.com>
2022-11-03 13:59:32 -0300

    criado rota para deletar obj

--- palestrinhas/urls.py
+++ palestrinhas/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import palestras
+from .views import palestras, palestra_delete
 
 
 urlpatterns = [
-    path('palestras/',palestras)
+    path('palestras/',palestras, name='palestras'),
+    path('palestras/<int:pk>/', palestra_delete, name='palestrasdelete')
 ]

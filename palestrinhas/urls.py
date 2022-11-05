ba5051419a476d62d8d9f2cb9054e483612aeee2
Carine Neris <carine_neris@hotmail.com>
2022-11-05 04:15:37 -0300

    criado update view

--- palestrinhas/urls.py
+++ palestrinhas/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import palestras, palestra_delete, palestra_create
+from .views import palestras, palestra_delete, palestra_create, palestra_detail
 
 
 urlpatterns = [
-    path('palestras/',palestras, name='palestras'),
-    path('palestra/create/',palestra_create,name='palestracreate'),
-    path('palestra/<int:pk>/',palestra_delete, name='palestradelete'),
+    path('palestras/', palestras, name='palestras'),
+    path('palestra/create/', palestra_create, name='palestracreate'),
+    path('palestra/<int:pk>/delete', palestra_delete, name='palestradelete'),
+    path('palestra/detail/<int:pk>/', palestra_detail, name='palestradetail'),
 ]
--- palestrinhas/views.py
+++ palestrinhas/views.py
@@ -8,9 +8,8 @@ from .forms import PalestraForm
 def palestras(request):
     palestras = Palestras.objects.all().order_by('-data_criacao')
     form = PalestraForm()
-    context = {'context': palestras,
-                'form': form}
-    return render(request,'palestras/index.html',context)
+    context = {'context': palestras, 'form': form}
+    return render(request, 'palestras/index.html', context)
 
 
 def palestra_create(request):
@@ -22,9 +21,26 @@ def palestra_create(request):
     return redirect('palestras')
 
 
-def palestra_delete(request,pk):
-    palestra =  Palestras.objects.get(pk=pk)
+def palestra_delete(request, pk):
+    palestra = Palestras.objects.get(pk=pk)
     palestra.delete()
     return redirect('palestras')
-  
-        
+
+
+def palestra_detail(request, pk):
+    if request.method == 'GET':
+        palestra = Palestras.objects.get(pk=pk)
+        form = PalestraForm(instance=palestra)
+        context = {'form': form,
+                   'palestra': palestra}
+        return render(request, 'palestras/palestra_edit.html', context)
+    if request.method == 'POST':
+        palestra = Palestras.objects.get(pk=pk)
+        form = PalestraForm(request.POST, instance=palestra)
+        if form.is_valid():
+            form.save()
+            return redirect('palestras')
+        else:
+            context = {'palestra': palestra,
+                       'form': form}
+            return render(request, 'palestras/palestra_edit.html', context)

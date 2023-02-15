e3f78aa11e6d828281f897bd6cd659635f92b1c8
Carine Neris <carine_neris@hotmail.com>
2023-02-15 10:37:25 -0300

    criada função no admin para que quando o usuario salvar a palestra pelo admin o user de cadastro seja o usuario logado

--- palestrinhas/admin.py
+++ palestrinhas/admin.py
@@ -1,6 +1,11 @@
-import site
 from django.contrib import admin
 from .models import Palestras
 
 
-admin.site.register(Palestras)
+@admin.register(Palestras)
+class PalestrasAdmin(admin.ModelAdmin):
+    exclude = ('user',)
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()

bd30cf4325cfbb9e61ed2df0eece7aace888fc91
Carine Neris <carine_neris@hotmail.com>
2023-08-26 22:32:28 -0300

    link de logout

--- palestrinhas/urls.py
+++ palestrinhas/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 from palestrinhas import views
 
 
@@ -7,5 +9,9 @@ urlpatterns = [
     path('palestra/create/', views.palestra_create, name='palestracreate'),
     path('palestra/<int:pk>/delete', views.palestra_delete, name='palestradelete'),
     path('palestra/detail/<int:pk>/', views.palestra_detail, name='palestradetail'),
-    path('palestras/by-user/',views.palestras_by_user,name='palestrasuser')
+    path('palestras/by-user/',views.palestras_by_user,name='palestrasuser'),
+
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/logout/", auth_views.LogoutView.as_view(redirect_field_name='palestras')),
+
 ]

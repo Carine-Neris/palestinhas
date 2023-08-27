5c3b7ae323d768f9e3ebca8b0fef48b8199c567b
Carine Neris <carine_neris@hotmail.com>
2023-08-26 21:00:12 -0300

    ajustes na url patterns

--- palestrinhas/urls.py
+++ palestrinhas/urls.py
@@ -3,9 +3,9 @@ from palestrinhas import views
 
 
 urlpatterns = [
-    path('palestras/', views.palestras, name='palestras'),
+    path('', views.palestras, name='palestras'),
     path('palestra/create/', views.palestra_create, name='palestracreate'),
     path('palestra/<int:pk>/delete', views.palestra_delete, name='palestradelete'),
     path('palestra/detail/<int:pk>/', views.palestra_detail, name='palestradetail'),
-    path('palestra/by-user/',views.palestras_by_user,name='palestrasuser')
+    path('palestras/by-user/',views.palestras_by_user,name='palestrasuser')
 ]

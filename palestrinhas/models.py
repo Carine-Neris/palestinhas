d6869b0526a27987196a90e6f73ded0efd8690eb
Carine Neris <carine_neris@hotmail.com>
2024-11-10 00:55:39 -0300

    criado model para adicionar comentarios

--- palestrinhas/models.py
+++ palestrinhas/models.py
@@ -15,10 +15,20 @@ class Palestras(models.Model):
     data_criacao = models.DateField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES,
                               default='Já Vi')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.nome
 
     class Meta:
         verbose_name_plural = "Palestras"
+
+
+class Comentarios(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    palestra = models.ForeignKey(Palestras, on_delete=models.CASCADE)
+    texto = models.TextField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comentário de {self.user} na palestra {self.palestra}"
